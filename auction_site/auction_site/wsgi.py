37bd68862ca76f9abc6d22594c43e9f69cfb2b65
Tred <githut709@gmail.com>
2025-10-05 21:20:13 +0530

    Update wsgi.py

--- auction_site/auction_site/wsgi.py
+++ auction_site/auction_site/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
 
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auction_site.settings')
-
 application = get_wsgi_application()
